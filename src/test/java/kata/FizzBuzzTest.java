57efb855379b657943627e5cd9026abe68cae695
Xavier Pigeon <craftsman@chrysocode.io>
2019-04-18 11:54:45 +0200

    Refactor to make duplications appear.

--- src/test/java/kata/FizzBuzzTest.java
+++ src/test/java/kata/FizzBuzzTest.java
@@ -30,8 +30,8 @@ import static testasyouthink.TestAsYouThink.resultOf;
 class FizzBuzzTest {
 
     String fizzbuzzify(int number) {
-        if (number % 15 == 0) {
-            return "FizzBuzz";
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "Fizz" + "Buzz";
         } else if (number % 3 == 0) {
             return "Fizz";
         } else if (number % 5 == 0) {
